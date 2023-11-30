50973fb5b9b75da6ea1c9cacbceb562397dfb723
McKinley <benbatton82@gmail.com>
2023-11-30 09:30:21 +0300

    отредактирован код для удобства читаемости

--- Program.cs
+++ Program.cs
@@ -1,34 +1,38 @@
-﻿Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-M = 1; N = 5. -> "1, 2, 3, 4, 5"
-M = 4; N = 8. -> "4, 6, 7, 8"
-public class Task64 {    
-    static int[] numbers = new int[2];
-    public static void inputNumber(int index){
-        string stringNumber = Console.ReadLine();
-        bool isNumber = int.TryParse(stringNumber, out numbers[index]);
-        if(!isNumber){
-            Console.WriteLine("Введено не число");  
-            return; 
-        } 
-    }
-    public static string PrintNumbers(int start, int end){
-        if(start > end){
-            Console.WriteLine("Стартовое значение должно быть меньше крайнего");  
-            return "";
-        } 
-        if(start == end) return start.ToString();
-        return (start + "\t" + PrintNumbers(start + 1, end));
-    }
-    static public void Main(string[] args) {
-        Console.Write("Введите число M:" + "\t");
-        inputNumber(0);
-        Console.Write("Введите число N:" + "\t");
-        inputNumber(1);
-        Console.WriteLine(PrintNumbers(numbers[0], numbers[1]));
-    }
-}
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+// public class Task64 {    
+//     static int[] numbers = new int[2];
+//     public static void inputNumber(int index){
+//         string stringNumber = Console.ReadLine();
+//         bool isNumber = int.TryParse(stringNumber, out numbers[index]);
+//         if(!isNumber){
+//             Console.WriteLine("Введено не число");  
+//             return; 
+//         } 
+//     }
+//     public static string PrintNumbers(int start, int end){
+//         if(start > end){
+//             Console.WriteLine("Стартовое значение должно быть меньше крайнего");  
+//             return "";
+//         } 
+//         if(start == end) return start.ToString();
+//         return (start + "\t" + PrintNumbers(start + 1, end));
+//     }
+//     static public void Main(string[] args) {
+//         Console.Write("Введите число M:" + "\t");
+//         inputNumber(0);
+//         Console.Write("Введите число N:" + "\t");
+//         inputNumber(1);
+//         Console.WriteLine(PrintNumbers(numbers[0], numbers[1]));
+//     }
+// }
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//--------------------------------------------------------------------------------
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 // public class Task66 {    
@@ -58,6 +62,8 @@ public class Task64 {
 //         Console.WriteLine(SumNumbers(numbers[0], numbers[1]));
 //     }
 // }
+
+//-----------------------------------------------------------------------------------
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 //Даны два неотрицательных числа m и n.
